6b6d446d648b4a75b5b4e148954639b20e03cbc5
Alexandre Storelli <a_misc@storelli.fr>
2018-01-14 15:39:57 +0100

    switch artist and title

--- parsers/France_Radio FG.js
+++ parsers/France_Radio FG.js
@@ -15,6 +15,7 @@ module.exports = function(exturl, callback) {
 		} catch(e) {
 			return callback(e.message, null, null);
 		}
-		return callback(null, { artist: parsedResult["artist"], title: parsedResult["title"], cover: parsedResult["image"] }, corsEnabled);
+		// switch artist and title
+		return callback(null, { artist: parsedResult["title"], title: parsedResult["artist"], cover: parsedResult["image"] }, corsEnabled);
 	});
 }
