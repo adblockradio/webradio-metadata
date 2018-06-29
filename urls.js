eff557920f2083b0b0e0081b33bb0cf4c9a9039b
Alexandre Storelli <a_misc@storelli.fr>
2018-06-29 13:32:28 +0200

    update RMC & BFM

--- urls.js
+++ urls.js
@@ -7,7 +7,7 @@ module.exports = [
 		country: "France",
 		radios: [
 			{ "name": "Alouette", "url": "https://www.alouette.fr/player/onair.php" },
-			{ "name": "BFM Business", "url": "http://bfmbusiness.bfmtv.com/current-emission/3" },
+			{ "name": "BFM Business", "url": "https://bfmbusiness.bfmtv.com/current-emission/3" },
 			{ "name": "Chérie", "url": "https://players.nrjaudio.fm/wr_api/live/fr?act=get_plist&id_wr=190&cp=utf8&fmt=json", "parser": "France_NRJ" },
 			{ "name": "Djam Radio", "url": "http://www.djamradio.com/actions/infos.php" },
 			{ "name": "Europe 1", "url": "http://cdn1-europe1.new2.ladmedia.fr/var/europe1/storage/diffusions_europe1.js" },
@@ -28,7 +28,7 @@ module.exports = [
 			{ "name": "Radio Scoop Lyon", "url": "https://radioscoop.com/dyn/xhr.php" },
 			{ "name": "Rire et Chansons", "url": "http://www.rireetchansons.fr/webradios" },
 			{ "name": "RFM", "url": "http://directradio.rfm.fr/rfm/now/3" },
-			{ "name": "RMC", "url": "http://bfmbusiness.bfmtv.com/current-emission/1", "parser": "France_BFM Business" },
+			{ "name": "RMC", "url": "https://bfmbusiness.bfmtv.com/current-emission/1", "parser": "France_BFM Business" },
 			{ "name": "RTL", "url": "http://www.rtl.fr/direct/live-player-config.json" },
 			{ "name": "RTL2", "url": "https://www.rtl2.fr/direct" },
 			{ "name": "Skyrock", "url": "http://skyrock.fm/api/v3/player/onair" },
