e148c57237ccd946d90fa7b3ad8292cb4dd29d9d
Alexandre Storelli <a_misc@storelli.fr>
2019-01-15 17:05:29 +0100

    build bugfix

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,21 @@
 const path = require('path');
-//const webpack = require('webpack');
+const webpack = require('webpack');
 
 module.exports = {
 	entry: './index.js',
 	output: {
 		path: path.resolve(__dirname, 'build/'),
-		filename: 'webradio-metadata.js'
+		filename: 'webradio-metadata.js',
+		library: 'WebradioMetadata',
+		libraryTarget: 'umd',
 	},
-	/*plugins: [
+	plugins: [
 		new webpack.ContextReplacementPlugin(/file-stream-rotator$/, ctx => {
 			for (const x of ctx.dependencies) {
 				x.critical = false;
 			}
 		})
-	],*/
+	],
 	target: 'node',
 	mode: 'production'
 };
